729c8d73b6adc04465b38eb1306bdc884702a25d
Jeon <rainsy02@gmail.com>
2015-05-19 01:23:36 +0900

    php-image-resize and upload.* added

--- upload.php
+++ upload.php
@@ -1,16 +1,22 @@
 <?php
 
-// respose에 error메세지를 출력
-// error_reporting(E_ALL);
-// ini_set("display_errors", 1);
+//respose에 error메세지를 출력
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
 
-require "/var/www/html/vendor/JWT.php";
+require "./vendor/firebase/php-jwt/Firebase/PHP-JWT/Authentication/JWT.php";
+require "./vendor/eventviva/php-image-resize/src/ImageResize.php";
+
+function print_error($message)
+{
+	http_response_code(500);
+	print('{"Error":"'.$message.'"}');
+	exit();
+}
 
 if ( !file_exists ("upload.ini") )
 {
-	http_response_code(500);
-	print('upload.ini file is not exists');
-	return;
+	print_error('upload.ini file is not exists');
 }
 
 $json_a = json_decode(file_get_contents("upload.ini"), true);
@@ -20,25 +26,61 @@ $key = "secretKey";
 $directory = $_POST[directory];
 $init_directory = $init_json[directory][$directory];
 
-if($init_directory == null) {
-	http_response_code(500);
-	print("Error:Can not setting value '$directory'");
-	return;
-}
+if($init_directory == null)
+	print_error("Invalid_directory");
 
 $name = $directory."/";
+
 
 if($init_directory[Authorization] == true)
 {	
 	$header = apache_request_headers();
 	$authorization = str_replace("Bearer ", "", $header[Authorization]);
 	$jwt = (array) (JWT::decode($authorization, $key, array('HS256')));
-	$name = $name.$jwt[fk_kids];
+
+	$length = count($init_directory[Authorization]);
+
+	$isAuth = false;
+	foreach($init_directory[Authorization] as $value)
+	{
+		if(	$value == "kids" && $jwt[fk_kids] ) {
+			$isAuth = true;
+			$name = $name.$jwt[fk_kids];
+			break;
+		}
+		else if( $value == "parents" && $jwt[fk_parents] ) {
+			$isAuth = true;
+			$fk_kids = $_POST[fk_kids];
+			if( !$fk_kids )
+			{
+				print_error("Invalid_fk_kids");
+			}
+
+
+			// need to check parents_has_kids table
+			$name = $name.$fk_kids;
+			break;
+		}
+		else if ($value == "admin" && $jwt[fk_admin] ) {
+			$isAuth = true;
+			$fk_kids = $_POST[fk_kids];
+			if( !$fk_kids )
+				print_error("Invalid_fk_kids");
+
+			$name = $name.$fk_kids;
+			break;
+		}
+	}
+
+	if( $isAuth == false )
+		print_error("Invaild_token");
+}
+else
+{
 }
 
 if ($_FILES)
 {
-	var_dump($_FILES['filename']['type']);
 	switch($_FILES['filename']['type'])
 	{
 		case 'image/jpg':
@@ -46,23 +88,37 @@ if ($_FILES)
 		case 'image/gif': $ext='gif';  break;
 		case 'image/png': $ext='png';  break;
 		default:
-				http_response_code(500);
-				print("Error:Invaild_extension");
-				return;
+				print_error('{"Error":"Invalid_file"}');
 	}
 	$name = $name.".".$ext;
+
+	if($_POST[width] || $_POST[height])
+	{
+		use \Eventviva\ImageResize;
+		/*
+		$image = new ImageResize($_FILES['filename']['tmp_name']);
+
+		if($_POST[width])
+			$image->resizeToWidth($_POST[width]);
+		if($_POST[height])
+			$image->resizeToHeight($_POST[height]);
+
+		$image->save($_FILES['filename']['tmp_name']);
+		*/
+	}
+
 	$success = move_uploaded_file ($_FILES['filename']['tmp_name'], $name);
 
 	if($success == 0)
 	{
 		http_response_code(500);	
-		print ("Error:can not found filename");
+		print ('{"Error":"Fail_write"}');
 		return;
 	}
-	print ("Success! name : $name");
+	print ('{"filename":"'.$name.'"}');
 }
 else
 {
-	print ("Error");
+	print ('{"Error":"No_file"}');
 }
 ?>
